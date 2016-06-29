0b184897b963274600168b0bd9d38cac5e4293ac
Ryan Ernst <rernst76@gmail.com>
2016-06-29 13:37:17 -0400

    run.py working

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 import DS1X31
 import time
 import requests
-import datetime
+from datetime import datetime 
+import pytz
 
 test = DS1X31.DS1X31(0x48, one_shot = True)
 
 test.init()
 test.start()
 
+print 'Temperature data being collected and sent to http://192.168.168.179:3000...'
 while(True):
 	temp = test.getTemp()
-	r = requests.post('http://192.168.168.179:3000/temp', data = {'temp': temp, 'timestamp': datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')})
-	print temp
+	unaware = datetime.now()
+	tz = pytz.timezone('US/Eastern')
+	now = tz.localize(unaware)
+	r = requests.post('http://192.168.168.5:3000/temp', data = {'temp': temp, 'timestamp': now.strftime('%Y-%m-%dT%H:%M:%S%z')})
         time.sleep(10)
