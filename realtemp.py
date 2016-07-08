613443cf54cd8a94fed3a184b164e244d02405ea
Ryan Ernst <rernst76@gmail.com>
2016-07-08 09:57:58 -0400

    Update for Ryan

--- realtemp.py
+++ realtemp.py
@@ -7,17 +7,24 @@ import json
 
 print 'Real Temperature data being collected and sent to http://192.168.168.5:3000...'
 while(True):
-
+	temp = 0
 	apiKey = '60f317d055265f90f81f6157503f89a6'
 	owm = OWM(apiKey)
-	obs = owm.weather_at_id(5202765)
-	weather = obs.get_weather()
-	tempobj = weather.get_temperature('fahrenheit')
 	
-	temp = tempobj['temp']
+	try:
+		obs = owm.weather_at_id(5202765)#Murrysville
+		weather = obs.get_weather()
+		tempobj = weather.get_temperature('fahrenheit')
+		temp = tempobj['temp']
+	except:
+		obs = owm.weather_at_id(5206379)#Pittsburgh on error
+		weather = obs.get_weather()
+		temppbj = weather.get_temperature('fahrenheit')
+		temp = tempobj['temp']
 
+	print temp
 	unaware = datetime.now()
 	tz = pytz.timezone('US/Eastern')
 	now = tz.localize(unaware)
 	r = requests.post('http://192.168.168.5:3000/realtemp', data = {'temp': temp, 'timestamp': now.strftime('%Y-%m-%dT%H:%M:%S%z')})
-        time.sleep(60)
+        time.sleep(600)
